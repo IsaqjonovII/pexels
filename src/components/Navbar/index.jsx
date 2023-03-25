737631d6cbfe9fc8616c50c363ce6b730d06fc39
ilhomjon003 <ilhomjonisoqjonov2@gmail.com>
2023-03-25 10:36:37 +0500

    navbar is fixed

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -4,21 +4,23 @@ import c from "./style.module.css";
 
 const Navbar = () => {
   return (
-    <div className={`${c.navbar} flex`}>
-      <div className={c.nav__logo__wrapper}>
-        <Link to="/">
-          <img src={pxLogo} alt="" className={c.nav__logo} />
-        </Link>
-      </div>
+    <div className={c.navbar__container}>
+      <div className={`${c.navbar} flex`}>
+        <div className={c.nav__logo__wrapper}>
+          <Link to="/">
+            <img src={pxLogo} alt="" className={c.nav__logo} />
+          </Link>
+        </div>
 
-      <div className={`${c.user__profile} flex`}>
-        <Link to="/profile">
-          <img
-            className={c.user}
-            src="https://api.dicebear.com/5.x/adventurer/svg?size=48&hair=short16&seed=Aneka"
-            alt="user profile image"
-          />
-        </Link>
+        <div className={`${c.user__profile} flex`}>
+          <Link to="/profile">
+            <img
+              className={c.user}
+              src="https://api.dicebear.com/5.x/adventurer/svg?size=48&hair=short16&seed=Aneka"
+              alt="user profile image"
+            />
+          </Link>
+        </div>
       </div>
     </div>
   );
